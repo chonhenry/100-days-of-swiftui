5d6ec273c67abe37eb598968263bc9c922325fe1
chonhenry <chonhenry@gmail.com>
2022-12-11 13:05:33 -0500

    project 2 guess the flag

---
+++ 03_project_2_guesstheflag/03_project_2_guesstheflag/_3_project_2_guesstheflagApp.swift
@@ -0,0 +1,17 @@
+//
+//  _3_project_2_guesstheflagApp.swift
+//  03_project_2_guesstheflag
+//
+//  Created by Henry Chon on 12/11/22.
+//
+
+import SwiftUI
+
+@main
+struct _3_project_2_guesstheflagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
